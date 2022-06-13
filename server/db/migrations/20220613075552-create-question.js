e05786a3d9d45056d4b64f7e08037ec61247efbe
Dmitry <krasnovd@gmail.com>
2022-06-13 11:20:11 +0300

    dataBase

---
+++ server/db/migrations/20220613075552-create-question.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Questions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'Categories',
+          }
+        }
+      },
+      score: {
+        type: Sequelize.INTEGER
+      },
+      text: {
+        type: Sequelize.STRING
+      },
+      answer: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Questions');
+  }
+};
