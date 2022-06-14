2bf22b6158d60f7575242e6456f28dc75fa3bcf5
Milana <milanammk@gmail.com>
2022-06-14 17:38:04 +0300

    result

--- client/src/components/GamePage/GamePage.jsx
+++ client/src/components/GamePage/GamePage.jsx
@@ -2,20 +2,23 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Item from '../Item/Item';
 import style from './game.module.css';
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Button } from "react-bootstrap";
 import { useNavigate } from 'react-router-dom';
 
 
 export default function GamePage() {
+	const user = useSelector((state) => state.user);
   const dispatch = useDispatch()
   const [counts, setCounts] = useState(0);
 	const navigate = useNavigate();
 
   const [categories, setCategories] = useState([])
+	const [game, setGame] = useState(true)
 
 	const showResult = (counts) => {
-		return navigate('/result');
+		// return navigate('/result');
+		return setGame(false);
 	}
 
   useEffect(() => {
@@ -35,7 +38,12 @@ export default function GamePage() {
       	{categories ? categories.map((el) => <Item counts={counts} setCounts={setCounts} title={el.title} id={el.id} key={el.id} />) : '404'}
     	</table> 
 		</div>
-		<Button style={{"background":"red", "margin": "30px"}} variant="primary" onClick={()=> showResult(counts)}>Закончить игру</Button>   
+		<Button style={{"background":"red", "margin": "30px"}} variant="primary" onClick={()=> showResult(counts)}>Закончить игру</Button> 
+		{!game ? 
+		<div className={style.result_container}>
+		<h1>Ваш результат, {user.name}, {counts} очков!</h1>
+		<h1>Поздравляем!!!</h1>
+		</div> : <div></div>}  
 		</div>
   )
 }
