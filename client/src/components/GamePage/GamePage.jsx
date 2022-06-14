2b9d350948f65d460a64d631dc26110c93f703a5
Dima <schtanckevitch.dima@yandex.ru>
2022-06-14 16:25:32 +0300

    final

--- client/src/components/GamePage/GamePage.jsx
+++ client/src/components/GamePage/GamePage.jsx
@@ -2,19 +2,29 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Item from '../Item/Item';
 import style from './game.module.css';
+import { useDispatch } from 'react-redux'
+
 
 export default function GamePage() {
+  const dispatch = useDispatch()
+  const [counts, setCounts] = useState(0)
+
   const [categories, setCategories] = useState([])
-  
+
   useEffect(() => {
+    console.log(counts);
     axios.get('http://localhost:3001/quest')
-     .then((data) => setCategories(data.data))
-  }, []);
-  
-  return (
+    .then((data) => setCategories(data.data));
+    
+    axios.get('http://localhost:3001/quest/all')
+    .then((quest) => dispatch({type: 'SET_QUESTION', payload: {data: quest.data}}));
+  }, [counts])
+    
+
+  return (  
 		<div className={style.game_container}>
     	<table className={style.field_container}>
-      	{categories ? categories.map((el) => <Item title={el.title} id={el.id} key={el.id} />) : '404'}
+      	{categories ? categories.map((el) => <Item counts={counts} setCounts={setCounts} title={el.title} id={el.id} key={el.id} />) : '404'}
     	</table>
 		</div>
   )
--- server/routes/game/questRouter.js
+++ server/routes/game/questRouter.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const { Category } = require('../../db/models')
+const { Category, Question } = require('../../db/models')
 
 router.get('/', async (req, res) => {
   const nameCategory = await Category.findAll()
   res.json(nameCategory) 
+})
+
+router.get('/all', async (req, res) => {
+  const allQuestions = await Question.findAll()
+  res.json(allQuestions) 
 })
 
 module.exports = router
